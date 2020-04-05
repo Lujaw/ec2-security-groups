67684f56669ab90ab8a762d0bf29f6e48fc1aa35
Luja <>
2020-04-05 19:01:01 +1000

    TMC-01: removing IpPermission fields

--- src/services/ec2.js
+++ src/services/ec2.js
@@ -1,6 +1,7 @@
 'use strict';
 // Load the AWS SDK for Node.js
 const AWS = require('aws-sdk');
+const _ = require("lodash");
 
 // Load credentials and set region from JSON file
 AWS.config.update({ region: process.env.region });
@@ -8,9 +9,15 @@ AWS.config.update({ region: process.env.region });
 const listSecurityGroups = async (params) => {
   // have to instantiate EC2 inside the function for the aws-sdk-mock to function properly
   const ec2 = new AWS.EC2({ apiVersion: process.env.apiVersion });
-  const list = await ec2.describeSecurityGroups(params).promise();
+  let list = await ec2.describeSecurityGroups(params).promise();
+
+  // list.SecurityGroups = list.SecurityGroups.map(removeIpPermissionFields);
   return list;
 };
+
+// removes the IpPermission fields as they can be optional
+const removeIpPermissionFields = group =>
+  _.omit(group, "IpPermissions", "IpPermissionsEgress");
 
 module.exports = {
   listSecurityGroups
