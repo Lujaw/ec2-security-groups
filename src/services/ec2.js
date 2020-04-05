3747a246a49b62c582ff2ef28f7d835d18c694f1
Luja <>
2020-04-05 18:09:52 +1000

    TMC-01: minor refactor

--- src/services/ec2.js
+++ src/services/ec2.js
@@ -1,18 +1,17 @@
 'use strict';
-
 // Load the AWS SDK for Node.js
 const AWS = require('aws-sdk');
-// Load credentials and set region from JSON file
-AWS.config.update({region: 'ap-southeast-2'});
 
-// Create EC2 service object
-const ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
+// Load credentials and set region from JSON file
+AWS.config.update({ region: process.env.region });
 
 const listSecurityGroups = async (params) => {
-    const list = await ec2.describeSecurityGroups(params).promise();
-    return list;
-}
+  // have to instantiate EC2 inside the function for the aws-sdk-mock to function properly
+  const ec2 = new AWS.EC2({ apiVersion: process.env.apiVersion });
+  const list = await ec2.describeSecurityGroups(params).promise();
+  return list;
+};
 
 module.exports = {
-    listSecurityGroups
+  listSecurityGroups
 };
--- src/tests/handler.test.js
+++ src/tests/handler.test.js
@@ -3,18 +3,17 @@ const AWS_SDK = require("aws-sdk");
 const handler = require("../handlers/securityGroups");
 const securityGroupSample = require("./securityGroupSample.json");
 
-
 beforeAll(() => {
-    const EC2 = new AWS_SDK.EC2({ apiVersion: '2016-11-15' });
-    AWS_Mock.mock('EC2', 'describeSecurityGroups', securityGroupSample);
- });
+  const EC2 = new AWS_SDK.EC2({ apiVersion: process.env.apiVersion });
+  AWS_Mock.mock('EC2', 'describeSecurityGroups', securityGroupSample);
+});
 
 afterAll(() => {
-    AWS_Mock.restore();
+  AWS_Mock.restore();
 });
 
 test('it returns the list of the security group', async (done) => {
-    const test = await handler.list();
-    expect(test).toBe("Hello!");
-    done();
+  const test = await handler.list();
+  expect(test.body).toEqual(JSON.stringify(securityGroupSample, null, 2));
+  done();
 });
