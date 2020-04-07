c9fd49742094cc96b572d6ad68f99ad33e8fdfeb
Luja <>
2020-04-07 23:41:08 +1000

    TMC-01: making response JSON API 1.0 compliant

--- src/handlers/securityGroups.js
+++ src/handlers/securityGroups.js
@@ -1,15 +1,39 @@
 "use strict";
+const JSONAPISerializer = require("jsonapi-serializer").Serializer;
 const ec2 = require("../services/ec2");
 
-module.exports.list = async (event) => {
-  const list = await ec2.listSecurityGroups();
+const SecurityGroupSerializer = new JSONAPISerializer("securityGroups", {
+  attributes: [
+    "Description",
+    "GroupId",
+    "GroupName",
+    "IpPermissions",
+    "IpPermissionsEgress",
+    "OwnerId",
+    "Tags",
+    "VpcId",
+  ],
+});
 
-  return {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true,
-    },
-    body: JSON.stringify(list, null, 2),
-  };
+module.exports.list = async (event) => {
+  try {
+    const { SecurityGroups } = await ec2.listSecurityGroups();
+    const list = SecurityGroupSerializer.serialize(SecurityGroups);
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: JSON.stringify(list, null, 2),
+    };
+  } catch (error) {
+    console.error(error.message);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "Could not retrive the security group list",
+      }),
+    };
+  }
 };
